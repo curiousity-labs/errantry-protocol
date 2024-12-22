eae6558ef2d814edaf9ca993e571dfd4ef49bcbd
David Colon <38386583+Da-Colon@users.noreply.github.com>
2024-12-22 00:02:54 -0500

    feat: enhance Errantry contract with client registration and update .gitignore

--- contracts/src/Errantry.sol
+++ contracts/src/Errantry.sol
@@ -1,8 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
+import {IErrantry} from "./interfaces/IErrantry.sol";
+import {ErrandManager} from "./ErrandManager.sol";
+import {Lib} from "./libraries/Lib.sol";
 
-
-import "./Lib.sol";
 /**
  * Definitions:
  * - Errantry: the business of running errands
@@ -11,17 +12,43 @@ import "./Lib.sol";
  * - Errand Client: a person who requests errands to be run
  */
 
-contract Errantry {
+contract Errantry is IErrantry {
+    error ClientAlreadyRegistered();
+
+    mapping(address => Lib.Client) public clients;
+
+    constructor() {}
+
     /* >>>>>>>> errand runner functions <<<<<<< */
     function claimErrandPayment() external {}
 
     /* >>>>>>>> errand client functions <<<<<<< */
-    function registerNewClient() external {}
-    function postNewErrand() external {}
+    function registerNewClient() external {
+        if (clients[msg.sender].client != address(0)) {
+            revert ClientAlreadyRegistered();
+        }
+        clients[msg.sender] = Lib.Client({
+            client: msg.sender,
+            errandManager: new ErrandManager(),
+            smartAccount: address(0)
+        });
+    }
+
+    // function postNewErrand(Lib.PostNewErrandParams calldata params) external {
+    //     Lib.Errand({
+    //         payer: params.client,
+    //         runner: address(0),
+    //         expires: params.expires,
+    //         completed: false,
+    //         paid: false
+    //     });
+    // }
+
     function payErrandInvoice() external {}
 
     /* >>>>>>>> oracle functions <<<<<<< */
     function registerNewErrand() external {}
+
     function updateErrandStatus() external {}
 
     /* >>>>>>>> internal functions <<<<<<< */
