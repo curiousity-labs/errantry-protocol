614e47499273b931d49c3d701d0c1317b11ab873
David Colon <38386583+Da-Colon@users.noreply.github.com>
2024-12-28 06:32:04 -0500

    add entry point and oracle constants

--- contracts/ignition/modules/Errantry.ts
+++ contracts/ignition/modules/Errantry.ts
@@ -1,7 +1,9 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules"
+import { zeroAddress } from "viem"
 
 const ErrantryModule = buildModule("ErrantryModule", (m) => {
-  const errantry = m.contract("Errantry", [])
+  // args = [address entry point, address oracle address]
+  const errantry = m.contract("Errantry", [zeroAddress, zeroAddress])
   return { errantry }
 })
 
