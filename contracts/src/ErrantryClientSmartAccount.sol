54b494d76c44998db9099979c83f98ff4a0cd715
David Colon <38386583+Da-Colon@users.noreply.github.com>
2024-12-30 13:27:05 -0500

    formatting

--- contracts/src/ErrantryClientSmartAccount.sol
+++ contracts/src/ErrantryClientSmartAccount.sol
@@ -1,15 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
-import {SimpleAccount, IEntryPoint, PackedUserOperation, SIG_VALIDATION_SUCCESS} from "@account-abstraction/contracts/samples/SimpleAccount.sol";
+import {
+    SimpleAccount,
+    IEntryPoint,
+    PackedUserOperation,
+    SIG_VALIDATION_SUCCESS
+} from "@account-abstraction/contracts/samples/SimpleAccount.sol";
 import {IErrantryClientSmartAccount} from "./interfaces/IErrantryClientSmartAccount.sol";
 import {IErrandManager} from "./interfaces/IErrandManager.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract ErrantryClientSmartAccount is IErrantryClientSmartAccount {
-    constructor(
-        IEntryPoint _entryPoint,
-        address _trustedOracle
-    ) IErrantryClientSmartAccount(_entryPoint, _trustedOracle) {}
+    constructor(IEntryPoint _entryPoint, address _trustedOracle)
+        IErrantryClientSmartAccount(_entryPoint, _trustedOracle)
+    {}
 
     /* >>>>>>>> general external functions <<<<<<< */
     function payErrands() external override {
@@ -17,10 +22,11 @@ contract ErrantryClientSmartAccount is IErrantryClientSmartAccount {
     }
 
     /* >>>>>>>> overridden functions <<<<<<< */
-    function _validateSignature(
-        PackedUserOperation calldata userOp,
-        bytes32 userOpHash
-    ) internal override returns (uint256 validationData) {
+    function _validateSignature(PackedUserOperation calldata userOp, bytes32 userOpHash)
+        internal
+        override
+        returns (uint256 validationData)
+    {
         bytes memory _callData = userOp.callData;
         // Extract the function selector from the first 4 bytes of callData
         bytes4 functionSelector;
@@ -41,7 +47,10 @@ contract ErrantryClientSmartAccount is IErrantryClientSmartAccount {
     }
 
     /* >>>>>>>> internal functions <<<<<<< */
-    function _checkErrandFundBalance() internal {}
+    function _checkErrandFundBalance(address token, uint256 requiredAmount) internal view {
+        uint256 balance = IERC20(token).balanceOf(address(this));
+        require(balance >= requiredAmount, "Insufficient token balance for errands");
+    }
 
     function _payErrands() internal {}
 }
