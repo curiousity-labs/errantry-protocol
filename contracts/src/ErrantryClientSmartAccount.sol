56070162dabaebde990154f4d077a3ebbd1f0ef5
David Colon <38386583+Da-Colon@users.noreply.github.com>
2024-12-31 11:29:07 -0500

    remove comments

--- contracts/src/ErrantryClientSmartAccount.sol
+++ contracts/src/ErrantryClientSmartAccount.sol
@@ -36,8 +36,6 @@ contract ErrantryClientSmartAccount is SimpleAccount, OnlyOracle {
             }
             // @dev IDE warning: "Possible reentrancy in `ErrantryClientSmartAccount.payErrands(contract IErrandManager)`"
             // @dev We are confident that the `markErrandAsPaid` function is safe to call
-            // ignore reentrancy warning
-            // solhint-disable-next-line reentrancy
             errandManager.markErrandAsPaid(i);
             IERC20(token).safeTransfer(errand.runner, amount);
         }
