d6948d609dd1351ab50d4d4902ec1adbb6fc680e
David Colon <38386583+Da-Colon@users.noreply.github.com>
2024-12-31 23:24:18 -0500

    feat: implement custom error for onlyOracle modifier in OnlyOracle contract

--- contracts/src/OnlyOracle.sol
+++ contracts/src/OnlyOracle.sol
@@ -4,12 +4,14 @@ pragma solidity ^0.8.28;
 contract OnlyOracle {
     address internal TRUSTED_ORACLE;
 
+    error OnlyTrustedOracle();
+
     constructor(address _trustedOracle) {
         TRUSTED_ORACLE = _trustedOracle;
     }
 
     modifier onlyOracle() {
-        require(msg.sender == TRUSTED_ORACLE, "ErrantryUtils: only oracle");
+        require(msg.sender == TRUSTED_ORACLE, OnlyTrustedOracle());
         _;
     }
 }
