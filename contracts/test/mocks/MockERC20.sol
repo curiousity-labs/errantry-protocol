6b08a4deec9cc4a29f36fe04641deaaa2e7e06b7
David Colon <38386583+Da-Colon@users.noreply.github.com>
2025-01-02 10:46:49 -0500

    move mock files

--- contracts/test/mocks/MockERC20.sol
+++ contracts/test/mocks/MockERC20.sol
@@ -3,28 +3,20 @@ pragma solidity ^0.8.28;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-/**
- * @title MockERC20
- * @dev A simple ERC20 token implementation for testing purposes.
- */
 contract MockERC20 is ERC20 {
-    constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
-
-    /**
-     * @dev Mints `amount` tokens to `account`.
-     * @param account The account to mint tokens to.
-     * @param amount The amount of tokens to mint.
-     */
-    function mint(address account, uint256 amount) external {
-        _mint(account, amount);
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint8 decimals
+    ) ERC20(name, symbol) {
+        _mint(msg.sender, 10 ** 9 * 10 ** uint256(decimals)); // Mint 1 billion tokens to deployer
     }
 
-    /**
-     * @dev Burns `amount` tokens from `account`.
-     * @param account The account to burn tokens from.
-     * @param amount The amount of tokens to burn.
-     */
-    function burn(address account, uint256 amount) external {
-        _burn(account, amount);
+    function mint(address to, uint256 amount) external {
+        _mint(to, amount);
+    }
+
+    function burn(address from, uint256 amount) external {
+        _burn(from, amount);
     }
 }
